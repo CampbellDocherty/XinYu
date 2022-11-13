da44ccaccacddb59acf0af3e900d763bbf25e536
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-11-13 16:06:43 +0000

    Remove use of state from playpiem file

--- src/Playpiem/Playpiem.tsx
+++ src/Playpiem/Playpiem.tsx
@@ -1,27 +1,16 @@
-import { useEffect, useState } from 'react';
 import useGetLocationByIp from '../api/useGetLocation';
 import SunsetTime from './SunsetTime';
 
 const Playpiem = () => {
-  const [lng, setLng] = useState<string | null>(null);
-  const [lat, setLat] = useState<string | null>(null);
-
   const { data, isLoading, isSuccess } = useGetLocationByIp();
-
-  useEffect(() => {
-    if (data) {
-      const { loc: location } = data;
-      const [userLat, userLng] = location.split(',');
-      setLat(userLat);
-      setLng(userLng);
-    }
-  }, [data]);
 
   if (isLoading) {
     return <p>Locating...</p>;
   }
 
-  if (isSuccess && lat && lng) {
+  if (isSuccess) {
+    const { loc: location } = data;
+    const [lat, lng] = location.split(',');
     return <SunsetTime lat={lat} lng={lng} city={data.city} />;
   }
 

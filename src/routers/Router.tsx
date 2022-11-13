3506e2c2f30f936ae0f8c6bea06d257f5784623e
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-11-13 15:57:04 +0000

    Add route for night time

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import Playpiem from '../Playpiem';
+import Night from '../Playpiem/Night';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Playpiem />} />
+        <Route path="/night" element={<Night />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
